5378b3a32fa85a20b2ea2e7f18310a1a285fef52
John Fairhurst <johnfairh@gmail.com>
2018-02-16 21:14:38 +0000

    Grab all the Ruby headers

---
+++ ruby_headers.h
@@ -0,0 +1,9 @@
+#include <ruby/ruby.h>
+/* May as well grab all of these too ?*/
+#include <ruby/debug.h>
+#include <ruby/encoding.h>
+#include <ruby/io.h>
+#include <ruby/re.h>
+#include <ruby/thread.h>
+#include <ruby/version.h>
+#include <ruby/vm.h>
